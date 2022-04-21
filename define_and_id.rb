59f5e61b03434630f86fc970938c2e7298b0c321
Thomas Turner <Thomasturner482@yahoo.com>
2022-04-21 13:31:36 -0500

    Answers to define and id

--- define_and_id.rb
+++ define_and_id.rb
@@ -4,20 +4,33 @@
 # EXAMPLE
 # The upcase method is called on the string object "Hello World"
 # No arguments are passed; upcase has one clear job which is to capitalize all letters that exist in the String
-# The return value is "HI"
+# The return value is "HELLO WORLD"
 "Hello World".upcase
 
-
+# The include? method is called on the string "hello world"
+# The argument "hello" is being passed to the include? method. Include? is checking if "hello" is part of The
+# original string.
+# The return value is true.
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string "hello world"
+# The argument "hello" is being passed to the end_with? method. end_with? is checking if "hello" is The
+# last part of the original string.
+# The return value is false.
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string "hello world"
+# The argument "rld" is being passed to the include? method. end_with? is checking if "rld" is The
+# last part of the original string.
+# The return value is true.
 "Hello World".end_with?("rld")
 
-
+# The length method is called on the string "hello world"
+# No argument is being passed to the length method. Length is checking for how manny characters are part of The
+# original string.
+# The return value is 11.
 "Hello World".length
 
-
+# size is an alias of length. In this case they do the same thing.
+# the return value is 11.
 "Hello World".size
